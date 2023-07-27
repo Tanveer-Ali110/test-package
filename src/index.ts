13862142794628f2860158a4d522c44df4ddd5e1
Tanveer-Ali110 <ali.tanveer1012@gmail.com>
2023-07-27 12:45:25 +0500

    add function

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,9 @@
 export const test = (input: string) => {
     return typeof input === "string"
 }
+export const test1 = (input: string) => {
+    return typeof input === "string"
+}
 
 
 console.log("test")
