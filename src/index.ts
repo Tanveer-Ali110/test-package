c2733f841c5354f591d4c8e025575f92608468a6
Tanveer-Ali110 <ali.tanveer1012@gmail.com>
2023-07-31 18:23:10 +0500

    fix: add func

--- src/index.ts
+++ src/index.ts
@@ -2,3 +2,9 @@
 export const test = (input: string) => {
     return typeof input === "string"
 }
+export const test1 = (input: string) => {
+    return typeof input === "string"
+}
+export const test2 = (input: string) => {
+    return typeof input === "string"
+}
