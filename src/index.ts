1dd5b9eb05f3383426f69c4b40f7c6efe57eadeb
Tanveer-Ali110 <ali.tanveer1012@gmail.com>
2023-07-31 16:02:16 +0500

    fix: test

--- src/index.ts
+++ src/index.ts
@@ -6,7 +6,5 @@ export const test = (input: string) => {
 export const test1 = (input: string) => {
     return typeof input === "string"
 }
-export const test3 = (input: string) => {
-    return typeof input === "string"
-}
+
 
