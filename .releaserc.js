63ce3bf2dd9ba9e2b9a723fcdfc91d46ad863757
Tanveer-Ali110 <ali.tanveer1012@gmail.com>
2023-07-31 17:47:40 +0500

    fix: test

--- .releaserc.js
+++ .releaserc.js
@@ -3,13 +3,13 @@ export default {
   plugins: [
     "@semantic-release/commit-analyzer",
     "@semantic-release/release-notes-generator",
+    "@semantic-release/npm",
     [
       "@semantic-release/git",
       {
         "assets": ["package.json", "CHANGELOG.md"],
         "message": "chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}"
       }
-    ],
-    "@semantic-release/github",
+    ]
   ]
 }
