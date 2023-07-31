4ea8682c9ed91fc23bc21bfffbf83a75f6b05ebb
Tanveer-Ali110 <ali.tanveer1012@gmail.com>
2023-07-31 18:12:25 +0500

    feat: test

--- .releaserc.js
+++ .releaserc.js
@@ -7,7 +7,7 @@ export default {
     [
       "@semantic-release/git",
       {
-        "assets": ["package.json", "CHANGELOG.md"],
+        "assets": ["package.json"],
         "message": "chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}"
       }
     ]
